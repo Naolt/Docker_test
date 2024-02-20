47c86140f5b1a67c6d9ec26afaefe7236b11aa1e
naolt <naoltamrat11@gmai.com>
2024-02-20 18:07:13 +0300

    feat: setup docker

---
+++ main.py
@@ -0,0 +1,18 @@
+def fizzbuzz(num):
+	if not num % 3 and not num % 5:
+		return "FizzBuzz"
+	elif not num % 3:
+		return "Fizz"
+	elif not num % 5:
+		return "Buzz"
+	else:
+		return str(num)
+	
+
+def main():
+	num = int(input("Enter a number: "))
+	print(fizzbuzz(num))
+
+if __name__ == "__main__":
+	main()
+
